8296caa7209d9520c0f438cd12d98a882688aa90
Piotr Chowaniec <piotrchowaniec@poczta.fm>
2019-09-09 12:24:29 +0200

    finish

--- js/main.js
+++ js/main.js
@@ -1,19 +1,41 @@
 const spanText = document.querySelector(".text");
 const spnCursor = document.querySelector('.cursor')
-const txt =
-    "Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim, ipsam quidem. Ea perferendis mollitia sunt modi dolorem sapiente vitae, dolores nostrum asperiores nihil! Molestiae delectus cupiditate repellendus ipsam minus, eveniet quis quisquam sequi ullam hic minima earum, aut sunt nostrum! Laboriosam cupiditate quisquam amet saepe modi quae tenetur perspiciatis magnam!";
+const txt = ["Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim", "ipsam quidem. Ea perferendis mollitia sunt modi dolorem sapiente vitae", "dolores nostrum asperiores nihil! Molestiae delectus cupiditate repellendus ipsam minus, eveniet  aut sunt nostrum!", "Laboriosam cupiditate quisquam amet saepe modi", "quae tenetur perspiciatis magnam!"]
+let activeLetter = -15
+let activeText = 0
+
 
 let indexText = 0;
 const time = 50
 
 const addLetter = () => {
-    spanText.textContent += txt[indexText];
-    indexText++;
-    if (indexText == txt.length) clearInterval(indexTyping);
+    if (activeLetter >= 0){
+        spanText.textContent += txt[activeText][activeLetter];
+    }
+   activeLetter++
+   if (activeLetter === txt[activeText].length) {
+       activeText++
+       if (activeText === txt.length){
+           return
+       }
+       return setTimeout(()=> {
+           activeLetter = -15
+           spanText.textContent =''
+           addLetter()
+       }, 2000)
+   }
+   setTimeout(addLetter,100)
+
+
+
+
+    // indexText++;
+    // if (indexText == txt.length) clearInterval(indexTyping);
 };
 
 const cursorAnimation = () => {
     spnCursor.classList.toggle('active')
 }
-const indexTyping = setInterval(addLetter, time);
+addLetter()
+// const indexTyping = setInterval(addLetter, time);
 setInterval(cursorAnimation, 400)
