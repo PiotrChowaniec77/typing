79db1a1014c21fdab3e2f08dae497a8c5a44ffdf
Piotr Chowaniec <piotrchowaniec@poczta.fm>
2019-09-09 11:38:48 +0200

    v 02

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 const spanText = document.querySelector(".text");
+const spnCursor = document.querySelector('.cursor')
 const txt =
     "Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim, ipsam quidem. Ea perferendis mollitia sunt modi dolorem sapiente vitae, dolores nostrum asperiores nihil! Molestiae delectus cupiditate repellendus ipsam minus, eveniet quis quisquam sequi ullam hic minima earum, aut sunt nostrum! Laboriosam cupiditate quisquam amet saepe modi quae tenetur perspiciatis magnam!";
 
@@ -10,4 +11,9 @@ const addLetter = () => {
     indexText++;
     if (indexText == txt.length) clearInterval(indexTyping);
 };
+
+const cursorAnimation = () => {
+    spnCursor.classList.toggle('active')
+}
 const indexTyping = setInterval(addLetter, time);
+setInterval(cursorAnimation, 400)
